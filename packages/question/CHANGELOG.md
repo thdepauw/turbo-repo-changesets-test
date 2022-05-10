0ea9b1bf719f1e416c702e234b5952de9dfa49aa
Thomas De Pauw <thomas.depauw@xerius.be>
2022-05-10 15:05:21 +0200

    hotfix: remove offensive sentence

--- packages/question/CHANGELOG.md
+++ packages/question/CHANGELOG.md
@@ -1,5 +1,11 @@
 # question
 
+## 1.1.1
+
+### Patch Changes
+
+- Remove offencive sentence
+
 ## 1.1.0
 
 ### Minor Changes
--- packages/question/index.js
+++ packages/question/index.js
@@ -4,5 +4,4 @@ module.exports = whatsYourName;
 
 function whatsYourName() {
   console.log("What's your name?");
-  console.log("You look like a smart kid 👌");
 }
